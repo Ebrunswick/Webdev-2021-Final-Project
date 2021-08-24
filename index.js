5757ce3a7c2c39a8a25e15e4ca25e8a2c5a0a128
ebrunswick <ebruns2@pdx.edu>
2021-08-23 22:12:49 -0700

    Added form elements and css for it. No the form doesn't work, but it doesnt have to according to the documentation.

---
+++ index.js
@@ -0,0 +1,8 @@
+
+let submitButton = document.querySelector('button');
+
+submitButton.addEventListener('click', handleSubmitButton);
+
+function handleSubmitButton (event) {
+    alert("This is a message to tell you the contact button doesn't work.");
+};
